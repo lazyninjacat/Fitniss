0d5c3f84eaaf79dd5586b6a80c759fe792369441
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-08-08 12:35:50 -0700

    SQLite database integrated

--- Assets/CircuitController.cs
+++ Assets/CircuitController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Doozy.Engine.Nody;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,42 +8,62 @@ public class CircuitController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI CurrentExerciseName;
     [SerializeField] TextMeshProUGUI CurrentExerciseAmount;
+
+    private GraphController graphcontroller;
     private int circuitOrder;
 
     private DataService dataService;
 
     private int circuitCount;
+
+    private bool circuitComplete;
     
     // Start is called before the first frame update
     void Start()
     {
+        circuitComplete = false;
+
+        graphcontroller = FindObjectOfType<GraphController>();
         dataService = StartupScript.ds;
         circuitOrder = 1;
         circuitCount = dataService.GetCircuitCount();
-        Debug.Log("circuit count = " + circuitCount);
         CurrentExerciseName.text = dataService.GetExerciseName(circuitOrder);
         CurrentExerciseAmount.text = dataService.GetExerciseAmount(circuitOrder);
       
     }
 
+    public void StartButton()
+    {
+        circuitComplete = false;
+        circuitOrder = 1;
+        CurrentExerciseName.text = dataService.GetExerciseName(circuitOrder);
+        CurrentExerciseAmount.text = dataService.GetExerciseAmount(circuitOrder);
+    }
+
+
     public void DoneButton()
     {
         Debug.Log("Done button");
-        circuitOrder++;
-        
-        if (circuitOrder <= circuitCount)
+        if (!circuitComplete)
         {
-            CurrentExerciseName.text = dataService.GetExerciseName(circuitOrder);
-            CurrentExerciseAmount.text = dataService.GetExerciseAmount(circuitOrder);
+            circuitOrder++;
+
+            if (circuitOrder <= circuitCount)
+            {
+                CurrentExerciseName.text = dataService.GetExerciseName(circuitOrder);
+                CurrentExerciseAmount.text = dataService.GetExerciseAmount(circuitOrder);
+            }
+            else
+            {
+                CurrentExerciseName.text = "DONE!!!";
+                CurrentExerciseAmount.text = "";
+                circuitComplete = true;
+            }
         }
         else
         {
-            CurrentExerciseName.text = "DONE!!!";
-            CurrentExerciseAmount.text = "";
-        }
-        Debug.Log("circuit order = " + circuitOrder + " / " + circuitCount);
-
-
+            graphcontroller.GoToNodeByName("StartMenu");
+        }     
     }
 
     // Update is called once per frame
